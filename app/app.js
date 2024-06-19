53afb918e44636cee44473e2226fec69b078ddbf
GregorSchulze <gr.trash@t-online.de>
2024-06-19 16:34:42 +0200

    Update
    Testimonial ergänzt
    
    Design verbessert
    
    Typografie angepasst
    
    Farbmanagement und Hierarchie angepasst:

---
+++ app/app.js
@@ -0,0 +1,4 @@
+document.getElementById("emailButton").addEventListener("click", function () {
+  window.location.href =
+    "mailto:gr.schulze@t-online.de?subject=Betreff&body=Hier%20ist%20der%20Text%20der%20E-Mail";
+});
